bea37745e364cf1707ed0a946e51605863458d67
Darsh kumar <116168977+darsh609@users.noreply.github.com>
2024-09-30 18:21:57 +0530

    done

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://study-notion-project-mern-kohl.vercel.app/${token}`
+    const url = `https://study-notion-project-mern-kohl.vercel.app/update-pssword/${token}`
 
     await mailSender(
       email,
